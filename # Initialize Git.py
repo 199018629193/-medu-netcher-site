52033152f3fdfbdec693b292ab6be7d8350dc42d
DjeHuty imn DjedkAra <calmcdoug@gmail.com>
2025-12-23 18:12:14 -0600

    Initial commit

---
+++ # Initialize Git.py
@@ -0,0 +1,67 @@
+# Initialize Git
+git init
+git add .
+git commit -m "Initial commit"
+
+# Link to GitHub repo
+git remote add origin https://github.com/cd C:cd C:git push -u origin maint
+bash: cd: medu-netcher-template: No such file or directory
+Initialized empty Git repository in C:/Users/calmc/.git/
+warning: could not open directory 'AppData/Local/Application Data/': Permission denied
+warning: could not open directory 'AppData/Local/History/': Permission denied
+warning: could not open directory 'AppData/Local/Microsoft/Windows/INetCache/Content.IE5/': Permission denied
+warning: could not open directory 'AppData/Local/Microsoft/Windows/INetCache/Low/Content.IE5/': Permission denied
+warning: could not open directory 'AppData/Local/Microsoft/Windows/Temporary Internet Files/': Permission denied
+warning: could not open directory 'AppData/Local/Temp/WinSAT/': Permission denied
+warning: could not open directory 'AppData/Local/Temporary Internet Files/': Permission denied
+warning: could not open directory 'AppData/Roaming/com.adobe.dunamis/30bf5565-ab10-474a-b541-b39e24dfbe2c/v1/0/': Permission denied
+warning: could not open directory 'AppData/Roaming/com.adobe.dunamis/7bf4212a-365e-494d-bec2-25c9b31dde91/v1/0/': Permission denied
+warning: could not open directory 'Application Data/': Permission denied
+warning: could not open directory 'Cookies/': Permission denied
+warning: could not open directory 'Documents/My Music/': Permission denied
+warning: could not open directory 'Documents/My Pictures/': Permission denied
+warning: could not open directory 'Documents/My Videos/': Permission denied
+warning: could not open directory 'Local Settings/': Permission denied
+warning: could not open directory 'My Documents/': Permission denied
+warning: could not open directory 'NetHood/': Permission denied
+warning: could not open directory 'PrintHood/': Permission denied
+warning: could not open directory 'Recent/': Permission denied
+warning: could not open directory 'SendTo/': Permission denied
+warning: could not open directory 'Start Menu/': Permission denied
+warning: could not open directory 'Templates/': Permission denied
+warning: in the working copy of '.bash_history', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.config/jgit/config', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.docker/config.json', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.docker/daemon.json', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.docker/models/layout.json', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.docker/models/models.json', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.docker/windows-daemon.json', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.gitconfig', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.lesshst', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.lingma/bin/2.5.1/extension/main.js', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.lingma/bin/config.json', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.lingma/bin/env.json', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.lingma/cache/diagnosis.bin', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.lingma/extension/local/config.json', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.lingma/extension/local/mcp.json', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.lingma/lingma_mcp.json', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.lingma/logs/lingma.log', LF will be replaced by CRLF the next time Git touches it
+error: open(".local/share/wezterm/gui-sock-8712"): Function not implemented
+error: unable to index file '.local/share/wezterm/gui-sock-8712'
+fatal: adding files failed
+Author identity unknown
+
+*** Please tell me who you are.
+
+Run
+
+  git config --global user.email "calmcdoug@gmail.com"
+  git config --global user.name "DjeHuty imn DjedkAra"
+
+to set your account's default identity.
+Omit --global to set the identity only in this repository.
+
+fatal: unable to auto-detect email address (got 'calmc@DjehutyiaH.(none)')
+error: src refspec main does not match any
+error: failed to push some refs to 'https://github.com/199018629193/medu-netcher-site.git'
+
